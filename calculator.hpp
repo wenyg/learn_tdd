3705c521abe49e59719645fc21984817b46b577b
wenyg <yg_wen@126.com>
2019-12-10 22:07:10 +0800

    s4

--- calculator.hpp
+++ calculator.hpp
@@ -3,5 +3,8 @@
 
 std::string Add(std::string n1, std::string n2)
 {
-    return "3";
+    int num1 = std::stoi(n1);
+    int num2 = std::stoi(n2);
+    int result = num1 + num2;
+    return std::to_string(result);
 }
--- test/calculatorTest.cpp
+++ test/calculatorTest.cpp
@@ -8,7 +8,7 @@ TEST(ADD,basicAdd)
 {
     ASSERT_THAT(Add("1","2"), Eq("3"));
     ASSERT_THAT(Add("100","100"), Eq("200"));
-    ASSERT_THAT(Add("-100","50"), Eq("50"));
+    ASSERT_THAT(Add("-100","50"), Eq("-50"));
     ASSERT_THAT(Add("-100","-100"), Eq("-200"));
 
 }
