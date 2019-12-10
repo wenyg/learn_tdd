74225441123b58e73ef03f8634a2b7cf5f43460c
wenyg <yg_wen@126.com>
2019-12-10 21:34:53 +0800

    s2

--- test/calculatorTest.cpp
+++ test/calculatorTest.cpp
@@ -1,6 +1,13 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+using namespace testing;
+
+TEST(ADD,basicAdd)
+{
+    std::string result = Add("1", "2");
+    ASSERT_THAT(result, Eq("3"));
+}
 int main(int argc, char **argv)
 {
     testing::InitGoogleMock(&argc, argv);
