5f172fa2941547a691c34c9ed835ac143aa1d156
wenyg <yg_wen@126.com>
2019-12-10 22:03:58 +0800

    s3

--- test/calculatorTest.cpp
+++ test/calculatorTest.cpp
@@ -6,8 +6,11 @@ using namespace testing;
 
 TEST(ADD,basicAdd)
 {
-    std::string result = Add("1", "2");
-    ASSERT_THAT(result, Eq("3"));
+    ASSERT_THAT(Add("1","2"), Eq("3"));
+    ASSERT_THAT(Add("100","100"), Eq("200"));
+    ASSERT_THAT(Add("-100","50"), Eq("50"));
+    ASSERT_THAT(Add("-100","-100"), Eq("-200"));
+
 }
 int main(int argc, char **argv)
 {
