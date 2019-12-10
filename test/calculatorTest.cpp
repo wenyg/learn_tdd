ce5811eab7599fa910f917d2275492777b591e2f
wenyg <yg_wen@126.com>
2019-12-10 21:53:57 +0800

    s5

--- test/calculatorTest.cpp
+++ test/calculatorTest.cpp
@@ -6,8 +6,18 @@ using namespace testing;
 
 TEST(ADD,basicAdd)
 {
-    std::string result = Add("1", "2");
-    ASSERT_THAT(result, Eq("3"));
+    srand(0);
+    for(int i=0; i<100; i++)
+    {
+        int num1 = rand();
+        int num2 = rand();
+        int result = num1 + num2;
+
+        std::string snum1 = std::to_string(num1);
+        std::string snum2 = std::to_string(num2);
+        std::string sresult = Add(snum1, snum2);
+        ASSERT_THAT(sresult, Eq(std::to_string(result)));
+    }
 }
 int main(int argc, char **argv)
 {
