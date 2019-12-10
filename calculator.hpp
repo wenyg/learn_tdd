0906808d0780328fbec72d0ea37f1e1328dcfa4d
wenyg <yg_wen@126.com>
2019-12-10 22:17:32 +0800

    实现 大数相加

--- calculator.hpp
+++ calculator.hpp
@@ -3,8 +3,8 @@
 
 std::string Add(std::string n1, std::string n2)
 {
-    int num1 = std::stoi(n1);
-    int num2 = std::stoi(n2);
-    int result = num1 + num2;
+    long long num1 = std::stoll(n1);
+    long long num2 = std::stoll(n2);
+    long long result = num1 + num2;
     return std::to_string(result);
 }
