2bce5c96fd459bb085f647413ae693dd04602c7e
wenyg <yg_wen@126.com>
2019-12-10 22:24:33 +0800

    添加超级大数测试用例

--- test/calculatorTest.cpp
+++ test/calculatorTest.cpp
@@ -14,7 +14,21 @@ TEST(ADD,basicAdd)
 
 TEST(ADD,AddBigNumber)
 {
-    ASSERT_THAT(Add("1234567887654321","8765432112345678"), "9999999999999999");
+    ASSERT_THAT(Add("1234567887654321","8765432112345678"), Eq("9999999999999999"));
+}
+
+TEST(ADD,AddSuperBigNumber)
+{
+    std::stringstream num1, num2;
+    std::stringstream result;
+    for(int i=0; i<100; i++)
+    {
+        num1 << 1;
+        num2 << 2;
+        result << 3;
+    }
+
+    ASSERT_THAT(Add(num1.str(),num2.str()), Eq(result.str()));
 }
 int main(int argc, char **argv)
 {
